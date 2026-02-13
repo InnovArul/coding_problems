d039eadd3bdd18a4255b622e0b8cec71b77081b8
Arul <arul.csecit@ymail.com>
2026-02-13 01:29:25 +0100

    11feb2026 problem added

--- 2026/feb/10feb2026.py
+++ 2026/feb/10feb2026.py
@@ -1,18 +1,23 @@
-# https://leetcode.com/problems/longest-balanced-substring-i/submissions/1917479406
+# https://leetcode.com/problems/longest-balanced-subarray-i/submissions/1915049457
 
 class Solution:
-    def longestBalanced(self, s: str) -> int:
-        n = len(s)
-        res = 0
+    def longestBalanced(self, nums: List[int]) -> int:
+        max_len = 0
 
-        for i in range(n):
-            cnt = defaultdict(int)
-            for j in range(i, n):
-                cnt[s[j]] += 1
-                if len(set(cnt.values())) == 1:
-                    res = max(res, j - i + 1)
-                    
-        return res
+        for i in range(len(nums)):
+            odd = {}
+            even = {}
+
+            for j in range(i, len(nums)):
+                if nums[j] & 1:
+                    odd[nums[j]] = odd.get(nums[j], 0) + 1
+                else:
+                    even[nums[j]] = even.get(nums[j], 0) + 1
+
+                if len(odd) == len(even):
+                    max_len = max(max_len, j - i + 1)
+
+        return max_len
 
 # Time complexity: O(n^2)
 # Space complexity: O(n)
